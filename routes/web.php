8f46b2e8afcb02cc9a8282f5630a368df49d7fc7
NotLeizzy <liwayfernandez11@gmail.com>
2026-04-23 13:42:38 +0800

    commit updated routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -2,38 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return redirect('/products');
 });
 
+// Products
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products123', [ProductController::class, 'store']);
 Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-?>
 
-<?php
-
-use App\Http\Controllers\EmployeeController;
-
+// Employees
 Route::get('/employees', [EmployeeController::class, 'index']);
 Route::post('/employeesAdd', [EmployeeController::class, 'store']);
 Route::get('/employees/{id}/edit', [EmployeeController::class, 'edit']);
 Route::put('/employees/{id}', [EmployeeController::class, 'update']);
 Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
 
-?>
-
-<?php
-
-use App\Http\Controllers\CategoryController;
-
+// Categories
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/categoriesAdd', [CategoryController::class, 'store']);
 Route::get('/categories/{id}/edit', [CategoryController::class, 'edit']);
 Route::put('/categories/{id}', [CategoryController::class, 'update']);
 Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
-
-?>
